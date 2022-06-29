47b6cca251fb84a8b9da3ae738971945307577c0
eccotw8952 <pajiaduh2222@gmail.com>
2022-06-30 01:49:11 +0500

    testing https://reactjsexample.com/react-responsive-carousel-component-with-grid-layout-to-easily-create-a-carousel-like-photo-gallery/ slider

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,38 +1,41 @@
 import React from 'react';
+import Carousel from 'better-react-carousel';
 
-import { IoMdArrowDropright, IoMdArrowDropleft } from 'react-icons/io';
+const dataSlide = [
+  {
+    title: 'man`s',
+    desc: "don't composise on style! get flat 30% off for new arrivals",
+    img: './img/slide-1.jpg',
+  },
+  {
+    title: 'woman`s',
+    desc: "don't composise on style! get flat 30% off for new arrivals",
+    img: './img/slide-2.jpg',
+  },
+  {
+    title: 'shop info',
+    desc: "don't composise on style! get flat 30% off for new arrivals",
+    img: './img/slide-3.jpg',
+  },
+];
 
 const Slider = () => {
   return (
-    <div className='slider-container container hidden'>
-      <div className='arrow-border arrow-left' direction='left'>
-        <IoMdArrowDropleft size={28} />
-      </div>
-
-      <div className='slide-wrapper'>
-        <div className='img-container'>
-          <img src='./img/slide-1.jpg' alt='' />
-        </div>
-        <div className='info-container'>
-          <h1 className='info-title'>summer sal</h1>
-          <p className='info-desc'>don't composise on style! get flat 30% off for new arrivals</p>
-          <button className='btn-primary info-btn '>show now</button>
-        </div>
-      </div>
-
-      <div className='slide-wrapper'>
-        <div className='img-container'>
-          <img src='./img/slide-1.jpg' alt='' />
-        </div>
-        <div className='info-container'>
-          <h1 className='info-title'>summer sal</h1>
-          <p className='info-desc'>don't composise on style! get flat 30% off for new arrivals</p>
-          <button className='btn-primary info-btn '>show now</button>
-        </div>
-      </div>
-      <div className='arrow-border arrow-right' direction='right'>
-        <IoMdArrowDropright size={28} />
-      </div>
+    <div className='slider-container'>
+      <Carousel cols={1} rows={1} gap={5} loop>
+        {dataSlide.map(({ title, desc, img }) => (
+          <Carousel.Item>
+            <div className='carousel-wrapper flex'>
+              <img src={img} alt='' />
+              <div className='info-container'>
+                <h1 className='info-title'>{title}</h1>
+                <p className='info-desc'>{desc}</p>
+                <button className='btn-primary info-btn '>show now</button>
+              </div>
+            </div>
+          </Carousel.Item>
+        ))}
+      </Carousel>
     </div>
   );
 };
